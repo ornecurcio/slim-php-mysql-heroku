3ca8bbfbe505e6f63634a5fd53042c4d383ad1f6
ornecurcio <ornecurcio@gmail.com>
2022-10-24 20:28:01 -0300

    cambie mensaje

--- app/index.php
+++ app/index.php
@@ -38,7 +38,7 @@ $app->group('/usuarios', function (RouteCollectorProxy $group) {
   });
 
 $app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("Slim Framework 4 PHP");
+    $response->getBody()->write("Slim Framework 4 PHP de Ornelita");
     return $response;
 
 });
